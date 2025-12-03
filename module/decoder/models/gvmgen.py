6bca8050d3b72f3fea2307fc7c3236f173fbbc48
Phan Trung Thuận <thuantk000@gmail.com>
2025-12-03 11:49:06 +0700

    update

--- module/decoder/models/gvmgen.py
+++ module/decoder/models/gvmgen.py
@@ -82,6 +82,7 @@ class GVMGen(BaseGenModel):
                 device = 'cpu'
 
         lm = convert_to_linear8bit(load_lm_model(name, device='cuda:0'))
+        print(lm)
         compression_model = load_compression_model(name, device='cuda:1')
         if 'self_wav' in lm.condition_provider.conditioners:
             lm.condition_provider.conditioners['self_wav'].match_len_on_eval = True
