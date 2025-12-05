5df39a121aec1b103392c00c72ae7989f81a2960
Phan Trung Thuận <thuantk000@gmail.com>
2025-12-05 15:34:09 +0700

    update

--- module/encoder/model.py
+++ module/encoder/model.py
@@ -186,7 +186,8 @@ class ResidualAttentionBlock(nn.Module):
 
     def attention(self, x: torch.Tensor):
         self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
-        print(x.dtype, next(self.attn.parameters()).dtype)
+        # print(x.dtype, next(self.attn.parameters()).dtype)
+        print(self.attn)
         
         with torch.cuda.amp.autocast(enabled=False):
             return self.attn(x, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
