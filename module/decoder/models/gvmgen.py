d692a4c1c17505bc55510fb1db9aa7c15ab1e675
Phan Trung Thuận <thuantk000@gmail.com>
2025-12-05 13:21:52 +0700

    update

--- module/decoder/models/gvmgen.py
+++ module/decoder/models/gvmgen.py
@@ -326,6 +326,8 @@ class GVMGen(BaseGenModel):
                         [self.sample_rate] * ref_wav[0].size(0),
                         [None], [0.])
                 with self.autocast:
+                    print(prompt_tokens)
+                    print(attributes)
                     gen_tokens = self.lm.generate(
                         prompt_tokens, attributes,
                         callback=callback, max_gen_len=max_gen_len, **self.generation_params)
