e5b1b097a40ff7bbcd1dddfa72e595a7d51f617b
Phan Trung Thuận <thuantk000@gmail.com>
2025-12-06 17:32:58 +0700

    update

--- module/encoder/model.py
+++ module/encoder/model.py
@@ -398,22 +398,8 @@ class Transformer(nn.Module):
         self.resblocks = nn.Sequential(*[ResidualAttentionBlock(width, heads, attn_mask) for _ in range(layers)]).eval()
 
     def forward(self, x: torch.Tensor):
-        origin_device = next(self.resblocks.parameters()).device
-        for i, layer in enumerate(self.resblocks):
-            print(i)
-            if i % 2 == 0:
-                x = x.to('cuda:1')
-                layer = layer.to('cuda:1')
-                x = layer(x)
-            else:
-                x = x.to('cuda:0')
-                layer = layer.to('cuda:0')
-                x = layer(x)
-            gc.collect()
-            torch.cuda.empty_cache()
-
-        return x.to(origin_device)
-        # return self.resblocks(x)
+        with torch.no_grad():
+            return self.resblocks(x)
 
 class QFormerIntermediate(nn.Module):
     def __init__(self, hidden_size, intermediate_size):
