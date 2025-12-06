8c3c830c01e081c0a88c9822b01de8fc579e580b
Phan Trung Thuận <thuantk000@gmail.com>
2025-12-06 21:00:07 +0700

    update

--- module/decoder/models/gvmgen.py
+++ module/decoder/models/gvmgen.py
@@ -107,9 +107,9 @@ class GVMGen(BaseGenModel):
             else:
                 device = 'cpu'
 
-        # lm = convert_to_linear8bit(load_lm_model(name, device).half(), device).eval()
+        lm = convert_to_linear8bit(load_lm_model(name, device).half(), device).eval()
         # lm = convert_to_linear4bit(load_lm_model(name, device).half()).eval()
-        lm = load_lm_model(name, device).half().eval()
+        # lm = load_lm_model(name, device).half().eval()
         compression_model = load_compression_model(name, device).eval()
         if 'self_wav' in lm.condition_provider.conditioners:
             lm.condition_provider.conditioners['self_wav'].match_len_on_eval = True
